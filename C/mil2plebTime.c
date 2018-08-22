60619d78f0057ad1c68bbbc5a40c4d46856c1d4f
Chris Topher <programmingfire@gmail.com>
2018-08-21 20:31:32 -0500

    Create function to validate Hours and Minutes

---
+++ C/mil2plebTime.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void validHours(int *x);
+void validMinutes(int *x);
+
+int main(){
+
+	int time = 2350;
+	int *ptrTime = &time;
+
+	validHours(ptrTime);		
+	validMinutes(ptrTime);		
+
+	return 0;
+}
+
+void validHours(int *x)
+{	
+	if(*x > 2359)
+	       printf("not a valid time --- exiting\n");	
+	else
+		printf("%d\n", *x);
+}
+
+void validMinutes(int *x)
+{
+	int minutes = *x % 100;
+	
+	if(minutes > 59)
+		printf("%d\n", "Minutes Invalid --- exiting\n");
+
+}
