af2dbcd4a0a5f635654069f8f4a96d5792b68507
Chris Topher <programmingfire@gmail.com>
2018-09-23 16:41:02 -0500

    Create functions to solve magic square

--- C/magicSquareSolver.c
+++ C/magicSquareSolver.c
@@ -2,8 +2,10 @@
 #include<math.h>
 
 int* makeMagicSquare(int);
+int* buildMagicSquare(int, int);
 int getMidIndex(int);
 int getMinIndex(int, int);
+int getNextIndex(int, int, int);
 
 int main()
 {
@@ -17,11 +19,14 @@ int main()
 
 int* makeMagicSquare(int sideLength) 
 {
-	int magicSquare [sideLength * sideLength];
+//	int magicSquare [sideLength * sideLength];
+	int * magicSquare = NULL;
 	int midIndex = getMidIndex(sideLength);
 	int minIndex = getMinIndex(midIndex, sideLength);
 
 	printf("%d\n", minIndex);
+
+	magicSquare = buildMagicSquare(sideLength, minIndex);
 
 	return magicSquare;
 }
@@ -33,9 +38,95 @@ int getMidIndex(int sideLength)
 	return midIndex;
 }
 
-int getMinIndex(int midIndex, int sideLength)
+int getMinIndex(int sideLength, int midIndex)
 {
 	int minIndex = midIndex + sideLength;
 	
 	return minIndex;
+}
+
+int* buildMagicSquare(int sideLength, int minIndex)
+{
+	int magicSquare[sideLength * sideLength];
+	int curIndex = minIndex;
+	int iterator = 0;
+//	magicSquare[minIndex] = 1;
+
+
+	for (int i = 1; i < ((sideLength * sideLength + 1)); i++)
+	{
+		magicSquare[curIndex] = i;
+
+		curIndex = getNextIndex(sideLength, curIndex, iterator);
+
+		if(iterator != sideLength)
+		{
+			++iterator;
+		}
+		else
+		{
+			iterator = 0;
+		}
+
+	}
+
+	return magicSquare;
+}
+
+int getNextIndex(int sideLength, int curIndex, int iterator)
+{
+	/*             3                  SIDE LEGNTH
+	 *             *
+	 *      --------------- 
+	 *      |      |      |
+	 *      0      1      2             BLOCK
+	 *      +      +      +
+	 *     /|\    /|\    /|\
+	 *    0 1 2  1 2 3  0 1 2          SUBINDEX
+	 *    = = =  = = =  = = =
+	 *    0 1 2  3 4 5  6 7 8           INDEX
+	 *  |[a,b,c,|d,e,f,|g,h,i]|         ARRAY
+	 * 
+	 */
+
+	int nextIndex; // We're returing this val
+	int block;
+	int subIndex;
+	int nextBlock;
+	int nextSubIndex;
+
+	block = (curIndex / sideLength);
+	subIndex = (curIndex % sideLength);
+
+	nextSubIndex = subIndex;              // Init this val equal to curSubIndex
+
+	//Get Next Block
+	if (block == sideLength - 1)
+	{
+		nextBlock = 0;
+	}
+	else
+	{
+		nextBlock = block + 1;
+	}
+
+	// Get Next SubIndex	
+	if (iterator != sideLength)
+	{
+		if (subIndex == sideLength - 1)
+		{
+			nextSubIndex = 0;
+		}
+		else
+		{
+			++nextSubIndex;
+		}
+	}
+
+	nextIndex = nextBlock + nextSubIndex;
+
+	return nextIndex;
+
+
+	
 }
