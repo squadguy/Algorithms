c47e084d80f5a44c1b2f56f84b838e5fa5244509
Chris Topher <programmingfire@gmail.com>
2018-09-23 19:47:24 -0500

    Comment code

--- C/magicSquareSolver.c
+++ C/magicSquareSolver.c
@@ -1,7 +1,35 @@
-#include<stdio.h>
-#include<math.h>
+/*********************************************************************
+ *                                                                   *
+ *                                                                   *
+ *                       MAGIC SQUARE SOLVER                         *
+ *                                                                   * 
+ *                   Solves for any odd sized side                   * 
+ *                                                                   *
+ *                          Chris Wampnar                            *
+ *                                                                   * 
+ *                                                                   *
+ *********************************************************************/
 
-//int* makeMagicSquare(int);
+/*
+ * This program uses an array to represent a square.
+ *
+ * Algorithm:
+ * # Logically divide array into "blocks" which represent square's rows, eqaul to side length
+ * # Find middle index of array (and hence, the "Square").
+ * # Move to the middle of the next "block" (sidelength distance away)
+ * # This index is the minimum number, or 1, always.
+ * # Now, we go from populate the array from number 2 to the Max number (sidelength * sidelength)
+ * # Initalize an interator to 1.
+ * # If interator isn't equal to sidelength - the next index is over one block and increase subindex by 1 ("wrap both block and index if you're all the way to the right")
+ * # If iterator is equal to side length - move over two blocks (wrap when needed) do not increase subindex
+ * # Continue to do this until all array values are full
+ * 
+ * This algorithm multiplies the next logical block value by the side length and then adds the current subindex +1 to determine the next value's index in the array.  See diagram in function "getNextIndex"
+ *
+ */
+
+#include<stdio.h>
+
 void buildMagicSquare(int);
 int getMidIndex(int);
 int getMinIndex(int, int);
@@ -10,29 +38,14 @@ int getNextIndex(int, int, int);
 int main()
 {
 
-	int sideLength = 5;
+	int sideLength = 21;
 
 	buildMagicSquare(sideLength);
 
 	return 0;
 }
 
-/*
-int* makeMagicSquare(int sideLength) 
-{
-//	int magicSquare [sideLength * sideLength];
-	int * magicSquare = NULL;
-	int midIndex = getMidIndex(sideLength);
-	int minIndex = getMinIndex(midIndex, sideLength);
-
-	printf("%d\n", minIndex);
-
-	magicSquare = buildMagicSquare(sideLength, minIndex);
-
-	return magicSquare;
-}
-*/
-
+//Finds Middle Index of Array
 int getMidIndex(int sideLength)
 {
 	int midIndex = ((sideLength * sideLength)/2);
@@ -40,6 +53,7 @@ int getMidIndex(int sideLength)
 	return midIndex;
 }
 
+//Determines where the Minimum Value (1) Goes
 int getMinIndex(int sideLength, int midIndex)
 {
 	int minIndex = midIndex + sideLength;
@@ -54,13 +68,11 @@ void buildMagicSquare(int sideLength)
 	int iterator = 1;
 	int magicSquare[sideLength * sideLength];
 
-
+	//Applies the values to the square
 	for (int i = 1; i < ((sideLength * sideLength + 1)); ++i )
 	{
+		//Set the value to the current index
 		magicSquare[curIndex] = i;
-
-		printf("%d:", magicSquare[curIndex]);
-		printf("%d\n", curIndex);
 
 		curIndex = getNextIndex(sideLength, curIndex, iterator);
 
@@ -76,12 +88,43 @@ void buildMagicSquare(int sideLength)
 
 	}
 
+//Print square
+	int total = 0;
+	for (int i = 0; i < (sideLength * sideLength); ++i)
+	{
+		if(i == 0)
+		{
+			printf("%d:", magicSquare[i]);
+			total = total + magicSquare[i];
+		}
+		else 
+			if(i % sideLength != 0 )
+			{
+				printf("%d:", magicSquare[i]);
+				total = total + magicSquare[i];
+			}
+			else
+			{
+				printf("   =%d", total);
+				total = 0;
+				printf("\n%d:", magicSquare[i]);
+
+				total = total + magicSquare[i];
+			}
+	}
+
+	printf("   =%d", total);
+	
+	printf("\n");
+
 }
 
 
 int getNextIndex(int sideLength, int curIndex, int iterator)
 {
-	/*             3                  SIDE LEGNTH
+	/*    Example Side Length = 3
+	 *
+	 *             3                  SIDE LEGNTH
 	 *             *
 	 *      --------------- 
 	 *      |      |      |
@@ -95,43 +138,43 @@ int getNextIndex(int sideLength, int curIndex, int iterator)
 	 * 
 	 */
 
-	int nextIndex; // We're returing this val
-	int block;
-	int subIndex;
+	int nextIndex;              // We're returing this val
+	int block;                  // This is a logical partition to represent a subArray 
+	int subIndex;               // The subindex in the logical subArray
 	int nextBlock;
 	int nextSubIndex;
 
-	block = (curIndex / sideLength);
-	subIndex = (curIndex % sideLength);
+	block = (curIndex / sideLength);       //Determine which logical block (subarray) we're in
+	subIndex = (curIndex % sideLength);    // Determine the subindex were at within the logical block
 
 
-	nextSubIndex = subIndex;              // Init this val equal to curSubIndex
+	nextSubIndex = subIndex;               // Init this val equal to curSubIndex
 
 	//Get Next Block
 	if (iterator != sideLength)
 	{
 		nextBlock = block + 1;
-		
+	        // Account and handle "wrap around" for blocks (last block "loops back" to first block)	
 		if (nextBlock >= sideLength)
 		{
 			nextBlock = nextBlock - sideLength;
 		}
 	}
-	else
+	else   //Jump right two blocks - do not increase subindex 
 	{
 		nextBlock = block + 2;
+	        // Account and handle "wrap around" for blocks (last block "loops back" to first block)	
 		if (nextBlock >= sideLength)
 		{
 			nextBlock = nextBlock - sideLength;
 		}
 	}
 
-	printf("%d\n", nextBlock);	
 
 	// Get Next SubIndex	
-	if (iterator != sideLength)
+	if (iterator != sideLength) // Needed for when iterator equals side length: if they are equal don't increase sub index
 	{
-		if (subIndex == sideLength - 1)
+		if (subIndex == sideLength - 1) //Subindex "wrap around" last subindex points to back to first
 		{
 			nextSubIndex = 0;
 		}
@@ -142,7 +185,6 @@ int getNextIndex(int sideLength, int curIndex, int iterator)
 	}
 
 	nextIndex = (nextBlock * sideLength) + nextSubIndex;
-//	printf("%d\n", nextIndex);
 
 	return nextIndex;
 	
