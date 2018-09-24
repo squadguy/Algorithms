4020efc971a5234fb595e7a7109e46e9334ffda7
Chris Topher <programmingfire@gmail.com>
2018-09-24 00:19:21 -0500

    Add space after the includes

--- C/reverseLL.c
+++ C/reverseLL.c
@@ -7,8 +7,8 @@
  *                                                             *
  ***************************************************************/
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct Node
 {
