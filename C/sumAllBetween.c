05db251bed89c79327bd199ed026db3a2557c5e6
Chris Topher <programmingfire@gmail.com>
2018-10-01 13:01:10 -0500

    Create three functions which sum values of a continous set of numbers

---
+++ C/sumAllBetween.c
@@ -0,0 +1,76 @@
+/* Finds the sum of a set */
+
+#include <stdio.h>
+
+int slow(int*, int*);
+int faster(int, int);
+int fastest(int*, int*);
+
+int main()
+{
+	int low = 6;
+	int high = 11;
+	int total;
+
+	total = slow(&low, &high);
+	printf("%d\n", total);
+	total = faster(low, high);
+	printf("%d\n", total);
+	total = fastest(&low, &high);
+	printf("%d\n", total);
+
+
+
+
+	return 0;
+}
+
+//Takes n iterations
+int slow(int* low, int* high)
+{
+	int sum = 0;
+
+	for( int i = *low; i < (*high+1); i++)
+	{
+		sum = sum + i; 
+	}
+
+
+	return sum;
+}
+
+/* Takes n/2 iterations */
+int faster(int low, int high)
+{
+	int sum = 0;
+
+	/* Handles sets with odd number of values */
+	if ((high - low)%2 != 0)
+	{
+		sum = high;
+		--high;
+	}
+
+	while(low != high)
+	{
+		sum += (low + high);
+		
+		++low;
+		--high;
+
+	}
+
+	sum = sum + low;
+
+	return sum;
+}
+
+/* Constant Time */
+int fastest(int* low, int* high)
+{
+	int length = (*high - *low)+1;
+
+	int sum = (int)(((double)(*high + *low) / 2) * length);
+
+	return sum;
+}
